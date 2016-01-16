d46e5f2c59231c26fca6848c8dbf1abf7bd70688
Andrew Tate <drewctate@gmail.com>
2016-01-16 09:05:25 -0700

    Scraper now finds surgeons recursively, starting from country/state

--- surgeons1/surgeons1/spiders/surgeons1.py
+++ surgeons1/surgeons1/spiders/surgeons1.py
@@ -1,15 +1,23 @@
 from __future__ import absolute_import
 import scrapy
+import time
 from surgeons1.items import SurgeonItem
 
 class DmozSpider(scrapy.Spider):
     name = "surgeons1"
     allowed_domains = ["surgery.org", "smartbeautyguide.com"]
     start_urls = [
-        "http://www.smartbeautyguide.com/select-surgeon?location=NJ,US",
+        "http://www.surgery.org/consumers/find-a-plastic-surgeon",
     ]
 
     def parse(self, response):
+        surgeonLists = []
+        for l in response.xpath('//div[@class="column left"]//a/@href').extract() + response.xpath('//div[@class="column right"]//a/@href').extract():
+            time.sleep(.2)
+            url = response.urljoin(l)
+            yield scrapy.Request(url, callback=self.parseSurgeonList)
+
+    def parseSurgeonList(self, response):
         for div in response.xpath('//div[@class="body vcard tether_stats-track-impress"]'):
             item = SurgeonItem()
             try:
@@ -27,4 +35,4 @@ class DmozSpider(scrapy.Spider):
         nextPage = response.xpath('//li[@class="pager-next pager-iteration"]/a/@href')
         if nextPage:
             url = response.urljoin(nextPage.extract()[0])
-            yield scrapy.Request(url, callback=self.parse)
+            yield scrapy.Request(url, callback=self.parseSurgeonList)
